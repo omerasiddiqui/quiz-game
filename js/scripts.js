3e8ebe5c386a0b890882a4a684a279b145dd798c
Omer Siddiqui <oasiddiqui1@gmail.com>
2018-05-30 21:48:49 -0400

    marvel api

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
 
     $('#btn2').click(function(){
-        $('.testing').fadeIn(1000)
+      const key = '1b67f606425f993d3fd9691fb6b64c3c&hash=20b5040f9ed9facc2ec64ccfa55528a3';
+        $.getJSON(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${key}`)
+            .done(function(res){
+                console.log(res.data);
+                const randomNum = Math.floor(Math.random() * 20);
+                const int = randomNum;
+                const data = res.data.results[int];
+                const name = data.name;
+                const description = data.description;
+                const moment = data.events.items
+                const url = data.urls[0].url;
+                const image = data.image;
+                console.log(moment.length);
+
+                if(moment.length < 3) {
+                    $('ul').html(`<li>Your character's name is ${name}. ${description}</li>`);
+                    $('.image').css("display", "none");
+                    $('.list').fadeIn(500);
+                } else {
+                    $('ul').html(`<li>Your character's name is ${name}. ${description} Most famous for ${moment[4].name}.</li>`);
+                    $('.image').css("display", "none");
+                    $('.list').fadeIn(500);
+                }
+            })
+            .fail(function(){
+                console.log('error!');
     })
+});
 
 
 
@@ -28,6 +54,7 @@ $(document).ready(function() {
 
         console.log(`Your character's name is ${name} and ${genderDisp()} is a ${species}. Originally from ${location} and currently ${status}.`)
         $('ul').html(`<li>Your character's name is ${name} and ${genderDisp()} is a ${species}. Originally from ${location} and currently ${status}.</li>`);
+        $('.image').css("display", "block");
         $('.image').attr('src', image);
         $('.list').fadeIn(500);
 
