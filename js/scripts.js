9f19202c5c3481b303621131f01def50bd823d45
Omer Siddiqui <oasiddiqui1@gmail.com>
2018-05-31 06:51:20 -0400

    general styles, added marvel search api, basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,122 @@
 $(document).ready(function() {
 
-    $('#btn2').click(function(){
+  $('form').on("submit", function(e) {
+    e.preventDefault();
+    const search = $('input[type=text]').val().trim();
+    $('input[type=text]').val('');
+    if (search.length > 0) {
+      console.log(search);
+
       const key = '1b67f606425f993d3fd9691fb6b64c3c&hash=20b5040f9ed9facc2ec64ccfa55528a3';
-        $.getJSON(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${key}`)
-            .done(function(res){
-                console.log(res.data);
-                const randomNum = Math.floor(Math.random() * 20);
-                const int = randomNum;
-                const data = res.data.results[int];
-                const name = data.name;
-                const description = data.description;
-                const moment = data.events.items
-                const url = data.urls[0].url;
-                const image = data.image;
-                console.log(moment.length);
+      $.getJSON(`https://gateway.marvel.com:443/v1/public/characters?name=${search}&ts=1&limit=20&apikey=${key}`)
+        .done(function(res) {
+          console.log(res);
+          console.log(res.data);
+          const randomNum20 = Math.floor(Math.random() * 20);
+          const int10 = Math.floor(Math.random() * 10);
+          const int5 = Math.floor(Math.random() * 5);
+          const int = randomNum20;
+          const data = res.data.results[0];
+          const name = data.name;
+          const description = data.description;
+          const moment = data.events.items
+          const url = data.urls[0].url;
+          const image = data.thumbnail.path + "/portrait_uncanny." + data.thumbnail.extension;
+          console.log(image);
 
-                if(moment.length < 3) {
-                    $('ul').html(`<li>Your character's name is ${name}. ${description}</li>`);
-                    $('.image').css("display", "none");
-                    $('.list').fadeIn(500);
-                } else {
-                    $('ul').html(`<li>Your character's name is ${name}. ${description} Most famous for ${moment[4].name}.</li>`);
-                    $('.image').css("display", "none");
-                    $('.list').fadeIn(500);
-                }
-            })
-            .fail(function(){
-                console.log('error!');
-    })
-});
+          if (moment.length < 1) {
+            $('ul').html(`<li>Your character's name is ${name}. ${description}</li>`);
+            $('.image').attr('src', image);
+            $('.list').css("opacity", "1");
+            $('.list').fadeIn(100);
+          } else {
+            $('ul').html(`<li>Your character's name is ${name}. ${description} Most famous for ${moment[int5].name}.</li>`);            $('.image').attr('src', image);
+            $('.list').css("opacity", "1");
+            $('.list').fadeIn(100);
+          }
+        })
+        .fail(function() {
+          console.log('error!');
+        })
+    };
+  })
 
-
-
-    $('#btn').click(function() {
-        $.getJSON('https://rickandmortyapi.com/api/character', function(res) {
-        console.log(res.results);
-        const randomNum = Math.floor(Math.random() * 20);
-        const int = randomNum;
-        const data = res.results[int];
-        const gender = data.gender;
+  //random character
+  $('#btn2').click(function() {
+    function randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    let int = randomIntFromInterval(1011334, 1009697);
+    console.log(int);
+    const key = '1b67f606425f993d3fd9691fb6b64c3c&hash=20b5040f9ed9facc2ec64ccfa55528a3';
+    $.getJSON(`https://gateway.marvel.com:443/v1/public/characters?id=${int}&ts=1&limit=20&apikey=${key}`)
+      .done(function(res) {
+        console.log(res);
+        console.log(res.data);
+        const int5 = Math.floor(Math.random() * 5);
+        console.log(int5);
+        const data = res.data.results[0];
         const name = data.name;
-        const species = data.species;
-        const location = data.location.name;
-        const status = data.status;
-        const image = data.image;
+        const description = data.description;
+        const moment = data.events.items
+        const url = data.urls[0].url;
+        const image = data.thumbnail.path + "/portrait_uncanny." + data.thumbnail.extension;
+        console.log(url);
 
-        function genderDisp() {
-        if(gender == "Male") {
-            return "he";
+        if (moment.length < 1) {
+          $('ul').html(`<li>Your character's name is ${name}. ${description}</li>`);
+          $('#fmi').html(`<h2>For More Info:`);
+          $('#url').attr("href", url).html(`<h2>Click Here</h2>`)
+          $('.image').attr('src', image);
+          $('.list').css("opacity", "1");
+          $('.list').fadeIn(100);
         } else {
-            return "she"
-        }}
+          $('ul').html(`<li>Your character's name is ${name}. ${description} Most famous for ${moment[int5].name}.</li>`);
+          $('#fmi').html(`<h2>For More Info:`);
+          $('#url').attr("href", url).html(`<h2>Click Here</h2>`)
+          $('.image').attr('src', image);
+          $('.list').css("opacity", "1");
+          $('.list').fadeIn(100);
+        }
+      })
+      .fail(function() {
+        console.log('error!');
+      })
+  });
 
-        console.log(`Your character's name is ${name} and ${genderDisp()} is a ${species}. Originally from ${location} and currently ${status}.`)
-        $('ul').html(`<li>Your character's name is ${name} and ${genderDisp()} is a ${species}. Originally from ${location} and currently ${status}.</li>`);
-        $('.image').css("display", "block");
-        $('.image').attr('src', image);
-        $('.list').fadeIn(500);
 
-        console.log(randomNum);
-        });
+
+
+
+  $('#btn').click(function() {
+    $.getJSON('https://rickandmortyapi.com/api/character', function(res) {
+      console.log(res.results);
+      const randomNum = Math.floor(Math.random() * 20);
+      const int = randomNum;
+      const data = res.results[int];
+      const gender = data.gender;
+      const name = data.name;
+      const species = data.species;
+      const location = data.location.name;
+      const status = data.status;
+      const image = data.image;
+
+      function genderDisp() {
+        if (gender == "Male") {
+          return "he";
+        } else {
+          return "she"
+        }
+      }
+
+      console.log(`Your character's name is ${name} and ${genderDisp()} is a ${species}. Originally from ${location} and currently ${status}.`)
+      $('ul').html(`<li>Your character's name is ${name} and ${genderDisp()} is a ${species}. Originally from ${location} and currently ${status}.</li>`);
+      // $('.image').css("display", "block");
+      $('.image').attr('src', image);
+      $('.list').css("opacity", "1");
+      $('.list').fadeIn(250);
+
+      console.log(randomNum);
     });
+  });
 });
